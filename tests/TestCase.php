aad1b10bc12cd3aad26eac8e675a65a2f642a60a
Ivan Vermeyen <ivan@vermeyen.org>
2020-08-02 15:09:20 +0200

    Cleanup TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,45 +3,10 @@
 namespace CodeZero\Translator\Tests;
 
 use CodeZero\Translator\TranslatorServiceProvider;
-use Illuminate\Foundation\Auth\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use RefreshDatabase;
-
-    /**
-     * Setup the test environment.
-     */
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->artisan('migrate', ['--database' => 'testing']);
-
-        $this->withFactories(__DIR__.'/../database/factories');
-
-        $this->be(new User());
-    }
-
-    /**
-     * Define environment setup.
-     *
-     * @param \Illuminate\Foundation\Application $app
-     *
-     * @return void
-     */
-    protected function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('database.default', 'testing');
-        $app['config']->set('database.connections.testing', [
-            'driver'   => 'sqlite',
-            'database' => ':memory:',
-            'prefix'   => '',
-        ]);
-    }
-
     /**
      * @param \Illuminate\Foundation\Application $app
      *
