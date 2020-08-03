c5aab13c7fa191524ddeea057c20a275659bb4b8
Ivan Vermeyen <ivan@vermeyen.org>
2020-08-03 17:51:23 +0200

    Don't use ValidatesRequests in controller

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -3,9 +3,8 @@
 namespace CodeZero\Translator\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class Controller extends BaseController
 {
-    use ValidatesRequests;
+    //
 }
